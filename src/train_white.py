d1e0584810baf6d19d5f43792b1b55f14f353d99
Sahil Joshi <sail.joshi0604@gmail.com>
2026-07-17 22:48:18 +0530

    Model training for WHITE WINE

---
+++ src/train_white.py
@@ -0,0 +1,30 @@
+from preprocessing import get_X_y
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestClassifier
+
+
+def train_model(df) :
+
+    X , y = get_X_y(df)
+
+    X_train , X_test , y_train , y_test = train_test_split(
+        X ,
+        y ,
+        test_size = 0.2 ,
+        stratify = y , 
+        random_state = 98
+    )
+
+    model = RandomForestClassifier()
+
+    model.fit(
+        X_train ,
+        y_train
+    )
+
+    model.fit(
+        X_train ,
+        y_train 
+    )
+
+    return model , X_test , y_test
